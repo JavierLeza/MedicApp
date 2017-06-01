f4c14cfafb75e2e7f60d48fb2b07fa33ec145b99
saraen <saraestravis@gmail.com>
2017-06-01 22:13:51 +0200

    Creación de la base de datos al iniciar la app

--- MedicApp/app/src/main/java/com/medic/medicapp/MainActivity.java
+++ MedicApp/app/src/main/java/com/medic/medicapp/MainActivity.java
@@ -1,13 +1,22 @@
 package com.medic.medicapp;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.medic.medicapp.data.MedicDbHelper;
+
 public class MainActivity extends AppCompatActivity {
+
+    public static SQLiteDatabase mDb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Creamos la BD
+        MedicDbHelper dbHelper = new MedicDbHelper(this);
+        mDb = dbHelper.getWritableDatabase();
     }
 }
