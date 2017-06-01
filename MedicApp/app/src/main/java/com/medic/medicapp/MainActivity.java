be285873dad3410b6387f78e09932025d5d7f82c
JavierLA <minikey13@gmail.com>
2017-06-01 23:31:24 +0200

    Avances
    Añadido botones de registro y acceso. Implementar la conexión entre el login y la pantalla inicial.

--- MedicApp/app/src/main/java/com/medic/medicapp/MainActivity.java
+++ MedicApp/app/src/main/java/com/medic/medicapp/MainActivity.java
@@ -1,14 +1,18 @@
 package com.medic.medicapp;
 
+import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.medic.medicapp.data.MedicDbHelper;
 
 public class MainActivity extends AppCompatActivity {
 
     public static SQLiteDatabase mDb;
+    private Button mLogInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +22,20 @@ public class MainActivity extends AppCompatActivity {
         //Creamos la BD
         MedicDbHelper dbHelper = new MedicDbHelper(this);
         mDb = dbHelper.getWritableDatabase();
+
+        mLogInButton = (Button) findViewById(R.id.b_log_in);
+
+        //Listener del botón LOGIN
+        mLogInButton.setOnClickListener(new View.OnClickListener() {
+
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,LogInActivity.class);
+
+                startActivity(intent);
+            }
+        });
     }
 }
