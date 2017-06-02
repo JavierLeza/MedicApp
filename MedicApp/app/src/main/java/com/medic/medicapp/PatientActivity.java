7b9022d444633b02304a880ddf3695adc4d133b2
saraen <saraestravis@gmail.com>
2017-06-02 20:48:32 +0200

    Lista de pacientes
    Implementación de la vista principal del médico al loggearse en su cuenta (sus pacientes)

--- MedicApp/app/src/main/java/com/medic/medicapp/PatientActivity.java
+++ MedicApp/app/src/main/java/com/medic/medicapp/PatientActivity.java
@@ -1,13 +1,148 @@
 package com.medic.medicapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.AsyncTaskLoader;
+import android.support.v4.content.Loader;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class PatientActivity extends AppCompatActivity {
+import com.medic.medicapp.data.MedicContract;
+
+
+public class PatientActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private PatientAdapter mAdapter;
+    public static String userName;
+    RecyclerView mRecyclerView;
+
+    // Constantes para logging y para referirse a un loader unico
+    private static final String TAG = PatientActivity.class.getSimpleName();
+    private static final int TASK_LOADER_ID = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient);
+
+        //Esto es para saber el nombre de usuario
+        Intent intentThatStartedThisActivity = getIntent();
+        if(intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)){
+            userName = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_new_list);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Al hacer click podemos añadir un nuevo paciente
+                /////////////////////Intent addListIntent = new Intent(PatientActivity.this, AddPatientActivity.class);
+                ////////////////////startActivity(addListIntent);
+            }
+        });
+
+        mRecyclerView = (RecyclerView) this.findViewById(R.id.recyclerViewLists);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+        Cursor cursor = getUserLists();
+        mAdapter = new PatientAdapter(this, cursor);
+        mRecyclerView.setAdapter(mAdapter);
+
+
+        getSupportLoaderManager().initLoader(TASK_LOADER_ID, null, this);
+    }
+
+    private Cursor getUserLists(){
+//        return mDb.query(
+//            //TODO: IMPLEMENTAR QUERY
+//        );
+        return null; //QUITAR DESPUES DE HACER EL TODO
+    }
+
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, final Bundle loaderArgs) {
+
+        return new AsyncTaskLoader<Cursor>(this) {
+
+            Cursor mListData = null;
+
+            // onStartLoading() is called when a loader first starts loading data
+            @Override
+            protected void onStartLoading() {
+                if (mListData != null) {
+                    // Delivers any previously loaded data immediately
+                    deliverResult(mListData);
+                } else {
+                    // Force a new load
+                    forceLoad();
+                }
+            }
+
+            // loadInBackground() performs asynchronous loading of data
+            @Override
+            public Cursor loadInBackground() {
+                // Will implement to load data
+
+                // Query and load all task data in the background; sort by priority
+
+                try {
+                    return getUserLists();
+
+                } catch (Exception e) {
+                    Log.e(TAG, "Failed to asynchronously load data.");
+                    e.printStackTrace();
+                    return null;
+                }
+            }
+
+            // deliverResult sends the result of the load, a Cursor, to the registered listener
+            public void deliverResult(Cursor data) {
+                mListData = data;
+                super.deliverResult(data);
+            }
+        };
+
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        // Update the data that the adapter uses to create ViewHolders
+        mAdapter.swapCursor(data);
+    }
+
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+
+        mAdapter.swapCursor(null);
+    }
+
+    //Este método se llama si esta actividad se pausa o se reinicia.
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getSupportLoaderManager().restartLoader(TASK_LOADER_ID, null, this);
+    }
+
+
+    public void listDetail(View view) {
+        //IMPLEMENTAR
     }
 }
--- MedicApp/app/src/main/java/com/medic/medicapp/PatientAdapter.java
+++ MedicApp/app/src/main/java/com/medic/medicapp/PatientAdapter.java
@@ -9,6 +9,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.medic.medicapp.data.MedicContract;
+
 public class PatientAdapter extends RecyclerView.Adapter<PatientAdapter.PatientViewHolder> {
 
     // cursor para mostrar los pacientes
@@ -20,25 +22,58 @@ public class PatientAdapter extends RecyclerView.Adapter<PatientAdapter.PatientV
      * cursor es un cursor a la BD para los pacientes que se mostrarán
      */
     public PatientAdapter(Context context, Cursor cursor) {
-
+        this.mContext = context;
+        this.mCursor = cursor;
     }
 
     @Override
     public int getItemCount() {
-
-        return 0;
+        if (mCursor == null) {
+            return 0;
+        }
+        return mCursor.getCount();
     }
 
     @Override
     public PatientViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Coge el layout del item  que rellena el Recycler View
 
-        return null; //CAMBIAR!!!!!!!!!!!!!!!!!!
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.patient_layout, parent, false);
+        return new PatientViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(PatientViewHolder holder, int position) {
+        // Movemos mCursor a la posición del item que se mostrará
+        mCursor.moveToPosition(position);
+
+        // Actualizar el view holder con la información que se necesita mostrar
+        String patientName = mCursor.getString(mCursor.getColumnIndex(MedicContract.PatientEntry.COLUMN_PATIENT_NAME));
+        final int id = mCursor.getInt(mCursor.getColumnIndex(MedicContract.PatientEntry.COLUMN_DNI));
+
+        // Mostrar el nombre de la lista
+        holder.itemView.setTag(id);
+        holder.listNameTextView.setText(patientName);
+
+    }
 
 
+    //Esta función intercambia el cursor que se mantiene en el Adapter con otro y fuerza que se refresque la interfaz
+    public Cursor swapCursor(Cursor newCursor) {
+
+        // comprueba si este cursor es el mismo que el anterior (mCursor)
+        if (mCursor == newCursor) {
+            return null; // porque no ha cambiado nada
+        }
+        Cursor temp = mCursor;
+        this.mCursor = newCursor; // nuevo cursor asignador¡
+
+        //comprobar si el cursor es valido, y si sí, actualizarlo
+        if (newCursor != null) {
+            this.notifyDataSetChanged();
+        }
+        return temp;
     }
 
 
@@ -53,7 +88,7 @@ public class PatientAdapter extends RecyclerView.Adapter<PatientAdapter.PatientV
 
         public PatientViewHolder(View itemView) {
             super(itemView);
-
+            listNameTextView = (TextView) itemView.findViewById(R.id.tv_patient_name);
         }
 
 
