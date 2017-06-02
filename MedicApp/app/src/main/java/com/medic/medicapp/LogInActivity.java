af589c5aa7bd5291e424d2040d89bd26357bbfdb
JavierLA <minikey13@gmail.com>
2017-06-02 18:44:59 +0200

    Añadido boton y campos
    Boton de acceder y campos de nombre de usuario y contraseña

--- MedicApp/app/src/main/java/com/medic/medicapp/LogInActivity.java
+++ MedicApp/app/src/main/java/com/medic/medicapp/LogInActivity.java
@@ -1,13 +1,21 @@
 package com.medic.medicapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class LogInActivity extends AppCompatActivity {
+
+    private EditText mUserNameEditText;
+    private EditText mPasswordEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+
+        mUserNameEditText = (EditText) findViewById(R.id.et_user_name);
+        mPasswordEditText = (EditText) findViewById(R.id.et_user_password);
     }
+
 }
