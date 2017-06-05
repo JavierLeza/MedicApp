c0a721a7249536c9a62bc02b48ab5bee101aa8d1
JavierLA <minikey13@gmail.com>
2017-06-05 19:21:24 +0200

    Implementación UserListAdapter

--- MedicApp/app/src/main/java/com/medic/medicapp/UserListAdapter.java
+++ MedicApp/app/src/main/java/com/medic/medicapp/UserListAdapter.java
@@ -1,8 +1,95 @@
 package com.medic.medicapp;
 
-/**
- * Created by JavierLA on 05/06/2017.
- */
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-public class UserListAdapter {
+import com.medic.medicapp.data.MedicContract;
+
+public class UserListAdapter extends RecyclerView.Adapter<UserListAdapter.UserViewHolder> {
+
+    // cursor para mostrar las listas.
+    private Cursor mCursor;
+    private Context mContext;
+
+    /**
+     * Contect es para la actividad que se llama
+     * cursor es un cursor a la BD para las listas que se mostrarán
+     */
+    public UserListAdapter(Context context, Cursor cursor) {
+        this.mContext = context;
+        this.mCursor = cursor;
+    }
+
+    @Override
+    public UserViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // Coge el layout del item  que rellena el Recycler View
+
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.user_item_layout, parent, false);
+        return new UserViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(UserViewHolder holder, int position) {
+        // Movemos mCursor a la posición del item que se mostrará
+        mCursor.moveToPosition(position);
+
+        // Actualizar el view holder con la información que se necesita mostrar
+        String userName = mCursor.getString(mCursor.getColumnIndex(MedicContract.UserEntry.COLUMN_USER_NAME));
+        final int id = mCursor.getInt(mCursor.getColumnIndex(MedicContract.UserEntry._ID));
+
+        // Mostrar el nombre de la lista
+        holder.itemView.setTag(id);
+        holder.userNameTextView.setText(userName);
+
+    }
+
+
+    @Override
+    public int getItemCount() {
+        if (mCursor == null) {
+            return 0;
+        }
+        return mCursor.getCount();
+    }
+
+    //Esta función intercambia el cursor que se mantiene en el Adapter con otro y fuerza que se refresque la interfaz
+    public Cursor swapCursor(Cursor newCursor) {
+
+        // comprueba si este cursor es el mismo que el anterior (mCursor)
+        if (mCursor == newCursor) {
+            return null; // porque no ha cambiado nada
+        }
+        Cursor temp = mCursor;
+        this.mCursor = newCursor; // nuevo cursor asignador¡
+
+        //comprobar si el cursor es valido, y si sí, actualizarlo
+        if (newCursor != null) {
+            this.notifyDataSetChanged();
+        }
+        return temp;
+    }
+
+    /**
+     * Clase interna para albergar las vistas que se necesitan para mostrar items individuales en la RecyclerView
+     */
+    class UserViewHolder extends RecyclerView.ViewHolder {
+
+        // Nombre de la lista
+        TextView userNameTextView;
+
+
+        public UserViewHolder(View itemView) {
+            super(itemView);
+            userNameTextView = (TextView) itemView.findViewById(R.id.tv_user_name);
+        }
+
+
+
+    }
 }
--- MedicApp/app/src/main/java/com/medic/medicapp/UsersListActivity.java
+++ MedicApp/app/src/main/java/com/medic/medicapp/UsersListActivity.java
@@ -1,13 +1,19 @@
 package com.medic.medicapp;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class UsersListActivity extends AppCompatActivity {
-
+    public static String userName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_users_list);
+        //Esto es para saber el nombre de admin
+        Intent intentThatStartedThisActivity = getIntent();
+        if(intentThatStartedThisActivity.hasExtra(Intent.EXTRA_TEXT)) {
+            userName = intentThatStartedThisActivity.getStringExtra(Intent.EXTRA_TEXT);
+        }
     }
 }
