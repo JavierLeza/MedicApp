bb98e550ded52070c17b01c9be3d5efbf0d9dcee
saraen <saraestravis@gmail.com>
2017-06-06 20:51:01 +0200

    Interfaz de admin account
    Cuando un admin entra en su cuenta para ver sus datos, esto es lo que ve

--- MedicApp/app/src/main/java/com/medic/medicapp/AdminAccountActivity.java
+++ MedicApp/app/src/main/java/com/medic/medicapp/AdminAccountActivity.java
@@ -1,7 +1,11 @@
 package com.medic.medicapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
 public class AdminAccountActivity extends AppCompatActivity {
 
@@ -9,5 +13,9 @@ public class AdminAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_admin_account);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 }
